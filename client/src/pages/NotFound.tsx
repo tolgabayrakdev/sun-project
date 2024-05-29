31b71435af11b46c46293b0d36f52091cc1bfbf9
tolgabayrakdev <bayraktolga28@gmail.com>
2024-05-29 10:47:27 +0300

    not found page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,5 +1,18 @@
+import { Flex, Text } from "@mantine/core";
+
 type Props = {};
 
-export default function NotFound({}: Props) {
-    return <div>404 - NotFound</div>;
+export default function NotFound({ }: Props) {
+    return (
+        <Flex h="100vh" justify="center" align="center" direction="column">
+            <Text
+                size="xl"
+                fw={900}
+                variant="gradient"
+                gradient={{ from: 'blue', to: 'gray', deg: 90 }}
+            >
+                404 - Page not found !
+            </Text>
+        </Flex>
+    )
 }
