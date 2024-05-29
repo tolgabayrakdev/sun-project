8b6202426f06f28fccbfe4e99e7c6c210e0a89d2
tolgabayrakdev <bayraktolga28@gmail.com>
2024-05-29 10:36:13 +0300

    layout

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -1,11 +1,42 @@
 import { Outlet } from 'react-router-dom';
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 type Props = {};
 
-export default function AppLayout({}: Props) {
+export default function AppLayout({ }: Props) {
+    const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+    const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
     return (
         <div>
-            <Outlet />
+
+            <AppShell
+                header={{ height: 60 }}
+                navbar={{
+                    width: 260,
+                    breakpoint: 'sm',
+                    collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+                }}
+                padding="md"
+            >
+                <AppShell.Header>
+                    <Group h="100%" px="md">
+                        <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+                        <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+                        <h3>Logo</h3>
+                    </Group>
+                </AppShell.Header>
+                <AppShell.Navbar p="md">
+                    {Array(3)
+                        .fill(0)
+                        .map((_, index) => (
+                            <Skeleton key={index} h={28} mt="sm" animate={false} />
+                        ))}
+                </AppShell.Navbar>
+                <AppShell.Main>
+                    <Outlet />
+                </AppShell.Main>
+            </AppShell>
         </div>
     );
 }
