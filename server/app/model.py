6d3fb45b1003aa379f3ef30ec1fd7dece38cc113
tolgabayrakdev <bayraktolga28@gmail.com>
2024-05-30 23:34:47 +0300

    models

--- server/app/model.py
+++ server/app/model.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import ForeignKey, Integer, String, DateTime, Column, Float
+from sqlalchemy import ForeignKey, Integer, String, DateTime, Column, Float, Numeric
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
@@ -13,11 +13,76 @@ class User(Base):
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
     role = relationship("Role", backref="roles")
+    subscriptions = relationship("Subscription", back_populates="user")
+    invoices = relationship("Invoice", back_populates="user")
 
 
 class Role(Base):
     __tablename__ = "roles"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(40), unique=True)
+    users = relationship("User", backref="roles")
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
+
+
+class Notification(Base):
+    __tablename__ = "notifications"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    message = Column(String(255), nullable=False)
+    sent_at = Column(DateTime, default=datetime.now())
+    user = relationship("User", back_populates="notifications")
+
+
+class AuditLog(Base):
+    __tablename__ = "audit_logs"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    action = Column(String(255), nullable=False)
+    timestamp = Column(DateTime, default=datetime.now())
+    user = relationship("User", back_populates="audit_logs")
+
+
+class Subscription(Base):
+    __tablename__ = "subscriptions"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    plan_id = Column(Integer, ForeignKey("plans.id"))
+    status = Column(String(50), nullable=False)
+    user = relationship("User", back_populates="subscriptions")
+    plan = relationship("Plan", back_populates="subscriptions")
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
+
+
+class Plan(Base):
+    __tablename__ = "plans"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(15), nullable=False)
+    description = Column(String(50))
+    price = Column(Numeric(10, 2), nullable=False)
+    duration = Column(Integer, nullable=False)
+
+
+class Invoices(Base):
+    __tablename__ = "invoices"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    subscription_id = Column(Integer, ForeignKey("subscriptions.id"))
+    price = Column(Numeric(10, 2), nullable=False)
+    status = Column(String(50), nullable=False)
+    user = relationship("User", back_populates="invoices")
+    payments = relationship("Payment", back_populates="invoices")
+    subscription = relationship("Subscription", back_populates="invoices")
+    issue_date = Column(DateTime)
+    due_date = Column(DateTime)
+
+
+class Payments(Base):
+    __tablename__ = "payments"
+    id = Column(Integer, primary_key=True, index=True)
+    invoice_id = Column(Integer, ForeignKey("invoices.id"))
+    amount = Column(Numeric(10, 2), nullable=False)
+    payment_method = Column(String(50))
+    payment_date = Column(DateTime)
--- server/backend-design.md
+++ server/backend-design.md
@@ -3,5 +3,35 @@
 ## Database tables;
 - Users
 - Roles
+- Notifications
+- Audit logs
 - Subscriptions
-- 
+- Plans
+- Invoices
+- Payments
+
+### Users process steps
+
+Kullanıcı hesap oluşturup ve bir üyelik aldığında, genellikle şu adımlar izlenir:
+
+1. Kullanıcı Hesabı Oluşturma:
+Kullanıcı yeni bir hesap oluşturur ve kayıt işlemi gerçekleştirilir.
+Bu adımda User modeli kullanılır.
+
+2. Abonelik Planının Seçilmesi:
+Kullanıcı bir abonelik planı seçer.
+Seçilen plana ait detaylar Plan modeli kullanılarak alınır.
+
+3. Ödeme İşlemi:
+Kullanıcı seçilen plan için bir ödeme gerçekleştirir.
+Ödeme detayları Invoice ve Payment modelleri kullanılarak kaydedilir.
+
+
+4. Abonelik Oluşturma:
+Kullanıcının seçtiği plana ait bir abonelik oluşturulur.
+Bu adımda Subscription modeli kullanılır.
+
+
+5. Denetim Günlüğü Kaydı:
+Tüm bu işlemler sırasında kullanıcı hareketleri denetim günlüğüne kaydedilir.
+Bu adımda AuditLog modeli kullanılabilir.
