69b236cd896c19281ea97042d73bb542c5ba85bf
tolgabayrakdev <bayraktolga28@gmail.com>
2024-06-07 10:26:04 +0300

    project config setup

--- node-server/src/app.ts
+++ node-server/src/app.ts
@@ -1,17 +1,25 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { Request, Response } from "express";
+import "dotenv/config";
 
 
 const app = express();
 
-app.use(morgan("short"))
+app.use(express.json());
+app.use(morgan("short"));
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
 
-app.get("/hello",(req: Request, res: Response) => {
+
+
+app.get("/hello", (req: Request, res: Response) => {
   res.send("Hello baby.")
 });
 
 app.listen(3000, () => {
   console.log("Server is running...");
-  
+
 });
