eb4d9c6b0fe8ee02b2c237b40583c1c798f22130
tolgabayrakdev <bayraktolga28@gmail.com>
2024-06-08 18:29:21 +0300

    backend notes

--- node-server/src/queries/auth-queries.ts
+++ node-server/src/queries/auth-queries.ts
@@ -1,6 +1,6 @@
 const loginQuery = `SELECT * FROM users WHERE email = $1 and password = $2`;
-const registerQuery = `INSERT INTO users(username ,email, password) 
-VALUES ($1, $2, $3)`;
+const registerQuery = `INSERT INTO users(email, password) 
+VALUES ($1, $2)`;
 const findByUsernameQuery = `SELECT * FROM users WHERE username = $1`;
 const findByEmailQuery = `SELECT * FROM users WHERE email = $1`;
 const verifyUserQuery = `SELECT id, username, email FROM users WHERE id = $1`;
