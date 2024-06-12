89405e2e562ec4fa234333e9b216a99ec02ab1f9
tolgabayrakdev <bayraktolga28@gmail.com>
2024-06-12 10:11:48 +0300

    settings page updated

--- client/src/pages/app/Settings.tsx
+++ client/src/pages/app/Settings.tsx
@@ -1,9 +1,58 @@
+import { Badge, Button, Card, Checkbox, Divider, Grid, Group, PasswordInput, Text, rem } from "@mantine/core"
+
 type Props = {}
 
-export default function Settings({}: Props) {
+export default function Settings({ }: Props) {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Grid p="md">
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <Card m="xs" style={{ minHeight: rem(180) }} shadow="sm" padding="lg" radius="md" withBorder>
+            <Group justify="space-between" mt="md" mb="xs">
+              <Text fw={500}>Şifre Güncelleme</Text>
+            </Group>
+            <Divider />
+            <PasswordInput
+              label="Parola"
+              placeholder="*******"
+              mt="md"
+            />
+            <PasswordInput
+              label="Parola Tekrarı"
+              placeholder="*******"
+              mt="xs"
+            />
+            <Button mt="md" type="submit" w="160">
+              Onayla
+            </Button>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <Card m="xs" style={{ minHeight: rem(180) }} shadow="sm" padding="lg" radius="md" withBorder>
+            <Group justify="space-between" mt="md" mb="xs">
+              <Text fw={500}>Bildirimler</Text>
+            </Group>
+            <Divider />
+
+            <Checkbox
+              mt="md"
+              defaultChecked
+              label="Mail bildirimleri"
+            />
+            <Checkbox
+              mt="md"
+              defaultChecked
+              label="Güvenlik bildirimleri"
+            />
+             <Checkbox
+              mt="md"
+              defaultChecked
+              label="Kullanıcı bildirimleri"
+            />
+          </Card>
+        </Grid.Col>
+      </Grid>
+
+    </>
   )
 }
